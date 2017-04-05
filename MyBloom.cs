c748c35bd65ba54964f9386fa5d3755533be435e
wumohai <wumohai@qq.com>
2017-04-05 14:30:53 +0800

    Bloom效果

---
+++ MyBloom.cs
@@ -0,0 +1,72 @@
+﻿ using UnityEngine;
+using System.Collections;
+
+public class MyBloom : MyPostEffectsBase {
+	public Shader bloomShader;
+	private Material bloomMat;
+	public Material material{
+		get{ 
+			bloomMat = CheckShaderAndCreateMaterial(bloomShader, bloomMat);
+			return bloomMat;
+		}
+	}
+	[RangeAttribute(1,8)]
+	public int iterations = 1;
+	
+	[RangeAttribute(0, 3.0f)]
+	public float BlurSpread = 0.5f;
+
+	[RangeAttribute(1,8)]
+	public int DownSample = 2;
+	// Use this for initialization
+
+	[RangeAttribute(0, 1)]
+	public float LuminanceThreshold = 0.5f;
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	/// <summary>
+	/// OnRenderImage is called after all rendering is complete to render image.
+	/// </summary>
+	/// <param name="src">The source RenderTexture.</param>
+	/// <param name="dest">The destination RenderTexture.</param>
+	void OnRenderImage(RenderTexture src, RenderTexture dest)
+	{
+		if (material != null)
+		{
+			material.SetFloat("_LuminanceThreshold", LuminanceThreshold);
+			int rtW = src.width / DownSample;
+			int rtH = src.height / DownSample;
+			RenderTexture buffer = RenderTexture.GetTemporary(rtW, rtH, 0);
+			buffer.filterMode = FilterMode.Bilinear;
+			Graphics.Blit(src, buffer, material, 0);
+			for (int i = 0; i < iterations; i++)
+			{
+				material.SetFloat("_BlurSize", 1.0f + i*BlurSpread);
+				RenderTexture buffer1 = RenderTexture.GetTemporary(rtW, rtH, 0);
+				Graphics.Blit(buffer, buffer1, material, 1);
+				RenderTexture.ReleaseTemporary(buffer);
+				buffer = buffer1;
+				buffer1 = RenderTexture.GetTemporary(rtW, rtH, 0);
+				Graphics.Blit(buffer, buffer1, material, 2);
+				RenderTexture.ReleaseTemporary(buffer);
+				buffer = buffer1;
+			}
+
+			material.SetTexture("_Bloom", buffer);
+			Graphics.Blit(src, dest, material, 3);
+			RenderTexture.ReleaseTemporary(buffer);
+
+
+		}
+		else{
+			Graphics.Blit(src, dest);
+		}
+	}
+}
---
+++ MyBloom.shader
@@ -0,0 +1,74 @@
+﻿Shader "Custom/MyBloom" {
+	Properties {
+		_MainTex ("Albedo (RGB)", 2D) = "white" {}
+	}
+	SubShader {
+		CGINCLUDE
+			#include "UnityCG.cginc"
+			fixed _BlurSize;
+			sampler2D _MainTex;
+			fixed3 _MainTex_TexelSize;
+			sampler2D _Bloom;
+			fixed _LuminanceThreshold;
+
+			struct v2f{
+				float4 pos:SV_POSITION;
+				half2 uv:TEXCOORD0;
+			};
+			v2f vertExtractBright(appdata_img v){
+				v2f o;
+				o.pos = mul(UNITY_MATRIX_MVP, v.vertex);
+				o.uv = v.texcoord;
+				return o;
+			}
+
+			fixed luminance(fixed4 color){
+				return 0.2125*color.r + 0.7154*color.g + 0.0721*color.b;
+			}
+
+			fixed4 fragExtractBright(v2f i):SV_Target{
+				fixed4 color = tex2D(_MainTex, i.uv);
+				fixed val = clamp(luminance(color) - _LuminanceThreshold, 0.0, 1.0); //clamp(x,a,b); x<a return a. x>b return b.else return x;
+				return color*val;
+			}
+
+			struct v2fBloom{
+				float4 pos:SV_POSITION;
+				half4 uv:TEXCOORD0;
+			};
+			v2fBloom vertexBloom(appdata_img v){
+				v2fBloom o;
+				o.pos = mul(UNITY_MATRIX_MVP, v.vertex);
+				o.uv.xy = v.texcoord;
+				o.uv.zw = v.texcoord;
+
+				#if UNITY_UV_STARTS_AT_TOP
+					if(_MainTex_TexelSize.y < 0.0){
+						o.uv.w = 1.0f -o.uv.w;
+					}
+				#endif
+				return o;
+			}
+			fixed4 fragBloom(v2fBloom i):SV_Target{
+				return tex2D(_MainTex, i.uv.xy) + tex2D(_Bloom, i.uv.zw);
+			}
+		ENDCG		
+
+		ZTest Always Cull Off ZWrite Off
+		Pass{
+			CGPROGRAM
+			#pragma vertex vertExtractBright
+			#pragma fragment fragExtractBright
+			ENDCG
+		}
+		UsePass "Custom/MyGaussianBlur/GAUSSIAN_BLUR_VERTICAL"
+		UsePass "Custom/MyGaussianBlur/GAUSSIAN_BLUR_HORIZONTAL"
+		Pass{
+			CGPROGRAM
+			#pragma vertex vertexBloom
+			#pragma fragment fragBloom
+			ENDCG
+		}
+	} 
+	FallBack "Diffuse"
+}
