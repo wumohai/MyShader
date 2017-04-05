9397ca8293e52d44b61d188c0235e3908c276e69
wumohai <wumohai@qq.com>
2017-04-05 15:01:02 +0800

    运动模糊

---
+++ MyMotionBlur.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+
+public class MyMotionBlur : PostEffectsBase {
+
+	public Shader motionShader;
+	private Material _MotionMaterial;
+	public Material motionMaterial{
+		get{
+			_MotionMaterial = CheckShaderAndCreateMaterial(motionShader, _MotionMaterial);
+			return _MotionMaterial;
+		}
+	}
+
+	[RangeAttribute(0.0f, 1f)]
+	public float blurAmount = 0.5f;
+	private RenderTexture accumulationTexture;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	/// <summary>
+	/// This function is called when the behaviour becomes disabled or inactive.
+	/// </summary>
+	void OnDisable()
+	{
+		DestroyImmediate(accumulationTexture);
+	}
+
+	/// <summary>
+	/// OnRenderImage is called after all rendering is complete to render image.
+	/// </summary>
+	/// <param name="src">The source RenderTexture.</param>
+	/// <param name="dest">The destination RenderTexture.</param>
+	void OnRenderImage(RenderTexture src, RenderTexture dest)
+	{
+		if (motionMaterial != null)
+		{
+			if (accumulationTexture == null || accumulationTexture.width != src.width || accumulationTexture.height != src.height)
+			{
+				DestroyImmediate(accumulationTexture);
+				accumulationTexture = new RenderTexture(src.width, src.height, src.depth);
+				// accumulationTexture.hideFlags = HideFlags.HideAndDontSave;
+				accumulationTexture.hideFlags = HideFlags.None;
+				Graphics.Blit(src, accumulationTexture);
+			}
+			accumulationTexture.MarkRestoreExpected();
+			motionMaterial.SetFloat("_BlurAmount", blurAmount);
+
+			Graphics.Blit(src, accumulationTexture, motionMaterial);
+			Graphics.Blit(accumulationTexture, dest);
+		}
+		else
+		{
+			Graphics.Blit(src, dest);
+		}
+	}
+}
---
+++ MyMotionBlur.shader
@@ -0,0 +1,45 @@
+﻿Shader "Custom/MyMotionBlur" {
+	Properties {
+		_MainTex ("Albedo (RGB)", 2D) = "white" {}
+	}
+	SubShader {
+		CGINCLUDE
+			#include "UnityCG.cginc"
+			sampler2D _MainTex;
+			fixed _BlurAmount;
+			struct v2f{
+				float4 pos:SV_POSITION;
+				half2 uv:TEXCOORD0;
+			};
+			v2f vert(appdata_img v){
+				v2f o;
+				o.pos = mul(UNITY_MATRIX_MVP, v.vertex);
+				o.uv = v.texcoord;
+				return o;
+			}
+			fixed4 fragRGB(v2f i):SV_Target{
+				return fixed4(tex2D(_MainTex, i.uv).rgb, _BlurAmount);
+			}
+			fixed4 fragA(v2f i):SV_Target{
+				return tex2D(_MainTex, i.uv);
+			}
+		ENDCG
+		ZWrite Off ZTest Always Cull Off
+		Pass{
+			Blend SrcAlpha OneMinusSrcAlpha
+			ColorMask RGB
+			CGPROGRAM
+				#pragma vertex vert
+				#pragma fragment fragRGB
+			ENDCG
+		}
+		Pass{
+			ColorMask A
+			CGPROGRAM
+				#pragma vertex vert
+				#pragma fragment fragA
+			ENDCG
+		}
+	} 
+	FallBack "Diffuse"
+}
